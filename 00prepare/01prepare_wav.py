5c63eeab21d4b734261b1186e2a9420f5d3ecf1c
Daiji Fukagawa <dfukagaw28@gmail.com>
2022-06-12 07:11:35 +0900

    Add colab_prepare.ipynb

--- 00prepare/01prepare_wav.py
+++ 00prepare/01prepare_wav.py
@@ -8,26 +8,32 @@
 # サンプリング周波数を変換するためのモジュール(sox)をインポート
 import sox
 
-# osモジュールをインポート
 import os
+from pathlib import Path
+import sys
+
+from tqdm import tqdm
 
 #
 # メイン関数
 #
-if __name__ == "__main__":
-    
+def main(data_dir='../data'):
+
+    if isinstance(data_dir, str):
+        data_dir = Path(data_dir)
+
     # wavファイルが展開されたディレクトリ
-    original_wav_dir = '../data/original/jsut_ver1.1/basic5000/wav'
+    original_wav_dir = data_dir / 'original/jsut_ver1.1/basic5000/wav'
 
     # フォーマット変換したwavファイルを出力するディレクトリ
-    out_wav_dir = '../data/wav'
+    out_wav_dir = data_dir / 'wav'
 
     # wavデータのリストを格納するディレクトリ
-    out_scp_dir = '../data/label/all'
+    out_scp_dir = data_dir / 'label/all'
 
     # 出力ディレクトリが存在しない場合は作成する
-    os.makedirs(out_wav_dir, exist_ok=True)
-    os.makedirs(out_scp_dir, exist_ok=True)
+    out_wav_dir.mkdir(parents=True, exist_ok=True)
+    out_scp_dir.mkdir(parents=True, exist_ok=True)
 
     # soxによる音声変換クラスを呼び出す
     tfm = sox.Transformer()
@@ -35,26 +41,25 @@ if __name__ == "__main__":
     tfm.convert(samplerate=16000)
 
     # wavデータのリストファイルを書き込みモードで開き，以降の処理を実施する
-    with open(os.path.join(out_scp_dir, 'wav.scp'), mode='w') as scp_file:
+    with open(out_scp_dir / 'wav.scp', mode='w') as scp_file:
         # BASIC5000_0001.wav ~ BASIC5000_5000.wav に対して処理を繰り返し実行
-        for i in range(5000):
+        for i in tqdm(range(5000)):
             filename = 'BASIC5000_%04d' % (i+1)
             # 変換元のオリジナルデータ (48000Hz)のファイル名
-            wav_path_in = os.path.join(original_wav_dir, filename+'.wav')
+            wav_path_in = original_wav_dir / (filename + '.wav')
             # 変換後のデータ(16000Hz)の保存ファイル名
-            wav_path_out = os.path.join(out_wav_dir, filename+'.wav')
+            wav_path_out = out_wav_dir / (filename + '.wav')
 
-            print(wav_path_in)
             # ファイルが存在しない場合はエラー
-            if not os.path.exists(wav_path_in):
-                print('Error: Not found %s' % (wav_path_in))
-                exit()
+            assert wav_path_in.exists()
 
             # サンプリング周波数の変換と保存を実行する
-            tfm.build_file(input_filepath=wav_path_in, 
-                           output_filepath=wav_path_out)
+            tfm.build_file(input_filepath=str(wav_path_in), 
+                           output_filepath=str(wav_path_out))
 
             # wavファイルのリストを書き込む
             scp_file.write('%s %s\n' % 
                            (filename, os.path.abspath(wav_path_out)))
-        
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
